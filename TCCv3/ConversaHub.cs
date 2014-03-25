65e1e71a92cf652f96bc315c5114238715f62400
saddambea <saddamm@gmail.com>
2014-03-24 22:21:42 -0300

    Commit completo.

---
+++ TCCv3/ConversaHub.cs
@@ -0,0 +1,36 @@
+﻿/*  Author: Bernardo M. Muller
+ *  Date: 24/03/2014 
+ *  Part of "Multiuser interaction with SignalR", a conclusion work
+ *  for obtaining a especialization title
+ *  Contact: saddamm@gmail.com
+ */
+
+using Microsoft.AspNet.SignalR;
+
+namespace TCCv3
+{
+    public class ConversaHub : Hub
+    {
+        public void SendPointInfo(string user, string text, 
+                                  string pointX, string pointY)
+        {
+            Clients.All.receivePointInfo(
+                user,
+                text,
+                pointX,
+                pointY
+              );
+        }
+
+        public void SendLineInfo(string point01X, string point01Y, 
+                                 string point02X, string point02Y)
+        {
+            Clients.All.sendLineInfo(
+                point01X,
+                point01Y,
+                point02X,
+                point02Y
+            );
+        }
+    }
+}
---
+++ TCCv3/Startup.cs
@@ -0,0 +1,21 @@
+﻿/*  Author: Bernardo M. Muller
+ *  Date: 24/03/2014 
+ *  Part of "Multiuser interaction with SignalR", a conclusion work
+ *  for obtaining a especialization title
+ *  Contact: saddamm@gmail.com
+ */
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TCCv3.Startup))]
+namespace TCCv3
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
